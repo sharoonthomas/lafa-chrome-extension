c03dbb8c33841e3ee1c9b81b9ffa8d7bb738cdce
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2016-01-26 20:02:05 -0800

    Initial version with no formal organization

---
+++ scripts/contentscript.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// check if the page is of any interest to lafa lafa before showing this
+console.log('\'Allo \'Allo! Content script');
+alert("Hey save some money, go to lafa lafa");
